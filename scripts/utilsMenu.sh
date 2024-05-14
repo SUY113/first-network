b476622304936cb7cbfcd202e1213786abd4810e
SUY113 <tntuan110322@gmail.com>
2024-05-14 15:45:51 +0700

    First commit

---
+++ scripts/utilsMenu.sh
@@ -0,0 +1,40 @@
+#/bin/bash
+
+function printHelp() {
+  echo "Usage: "
+  echo "  byfn.sh <mode> [-c <channel name>] [-t <timeout>] [-d <delay>] [-f <docker-compose-file>] [-s <dbtype>] [-l <language>] [-o <consensus-type>] [-i <imagetag>] [-a] [-n] [-v]"
+  echo "    <mode> - one of 'up', 'down', 'restart', 'generate' or 'upgrade'"
+  echo "      - 'up' - bring up the network with docker-compose up"
+  echo "      - 'down' - clear the network with docker-compose down"
+  echo "      - 'restart' - restart the network"
+  echo "      - 'generate' - generate required certificates and genesis block"
+  echo "      - 'upgrade'  - upgrade the network from version 1.3.x to 1.4.0"
+  echo "    -c <channel name> - channel name to use (defaults to \"mychannel\")"
+  echo "    -t <timeout> - CLI timeout duration in seconds (defaults to 10)"
+  echo "    -d <delay> - delay duration in seconds (defaults to 3)"
+  echo "    -f <docker-compose-file> - specify which docker-compose file use (defaults to docker-compose-cli.yaml)"
+  echo "    -s <dbtype> - the database backend to use: goleveldb (default) or couchdb"
+  echo "    -l <language> - the chaincode language: golang (default) or node"
+  echo "    -o <consensus-type> - the consensus-type of the ordering service: solo (default), kafka, or etcdraft"
+  echo "    -i <imagetag> - the tag to be used to launch the network (defaults to \"latest\")"
+  echo "    -a - launch certificate authorities (no certificate authorities are launched by default)"
+  echo "    -n - do not deploy chaincode (abstore chaincode is deployed by default)"
+  echo "    -v - verbose mode"
+  echo "  byfn.sh -h (print this message)"
+  echo
+  echo "Typically, one would first generate the required certificates and "
+  echo "genesis block, then bring up the network. e.g.:"
+  echo
+  echo "	byfn.sh generate -c mychannel"
+  echo "	byfn.sh up -c mychannel -s couchdb"
+  echo "        byfn.sh up -c mychannel -s couchdb -i 1.4.0"
+  echo "	byfn.sh up -l node"
+  echo "	byfn.sh down -c mychannel"
+  echo "        byfn.sh upgrade -c mychannel"
+  echo
+  echo "Taking all defaults:"
+  echo "	byfn.sh generate"
+  echo "	byfn.sh up"
+  echo "	byfn.sh down"
+}
+
